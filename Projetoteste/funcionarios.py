0a01436b86f418835030cbfd50381f27742a4ac9
JoaoJotha <87660259+absolutebr@users.noreply.github.com>
2022-07-27 17:16:01 -0300

    Delete Projetoteste directory

--- Projetoteste/funcionarios.py
+++
@@ -1,9 +0,0 @@
-print('************************************')
-print('    Tabela de preços de Paes')
-print('************************************')
-
-
-precoPao = 0.18
-
-for x in range(1, 51):
-    print(x ,'-', x * precoPao)
